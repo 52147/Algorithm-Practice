7c483ccd954ef3b629de09552d40ec1ff844b0ab
Shou-Tzu Han <debra4117@gmail.com>
2024-07-11 17:22:14 -0500

    update

--- Dynamic Programming/1D DP/Sequence Problems/JumpGameI.java
+++ Dynamic Programming/1D DP/Sequence Problems/JumpGameI.java
@@ -6,6 +6,8 @@ public class JumpGameI {
                 return false;
             }
             maxReach = Math.max(maxReach, i + nums[i]);
+            // The expression i + nums[i] in your code represents the furthest index you can reach from the current index i.
+
         }
         return true;
     }
@@ -18,3 +20,24 @@ public class JumpGameI {
         System.out.println(jumpGame.canJump(nums2)); // Output: false
     }
 }
+// class Solution {
+//     public int jump(int[] nums) {
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp, Integer.MAX_VALUE);
+//         dp[0] = 0; // dp[0], which is set to 0 since no jumps are needed to stay at the starting position.
+//         for(int i = 0; i < nums.length; i++){
+//             for(int j = 0; j < i; j++){
+//                 // The inner loop iterates over all positions j that precede i to check if it's possible to reach i from j.
+
+//                 if(nums[j] + j >= i){
+//                     dp[i] = Math.min(dp[j] + 1, dp[i]);
+//                     // dp[j] + 1 represents the number of jumps needed to reach position i
+//                     // does not mean that there is only one jump to choose from. 
+
+//                 }
+//             }
+//         }
+//         return dp[nums.length - 1];
+        
+//     }
+// }
--- Dynamic Programming/1D DP/Sequence Problems/JumpGameII.java
+++ Dynamic Programming/1D DP/Sequence Problems/JumpGameII.java
@@ -27,3 +27,24 @@ public class JumpGameII {
         System.out.println(jumpGame.jump(nums2)); // Output: 2
     }
 }
+// class Solution {
+//     public int jump(int[] nums) {
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp, Integer.MAX_VALUE);
+//         dp[0] = 0; // dp[0], which is set to 0 since no jumps are needed to stay at the starting position.
+//         for(int i = 0; i < nums.length; i++){
+//             for(int j = 0; j < i; j++){
+//                 // The inner loop iterates over all positions j that precede i to check if it's possible to reach i from j.
+
+//                 if(nums[j] + j >= i){
+//                     dp[i] = Math.min(dp[j] + 1, dp[i]);
+//                     // dp[j] + 1 represents the number of jumps needed to reach position i
+//                     // does not mean that there is only one jump to choose from. 
+
+//                 }
+//             }
+//         }
+//         return dp[nums.length - 1];
+        
+//     }
+// }
